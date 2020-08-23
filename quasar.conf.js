e0e45e569b8e3d3d49a87bf48b54dc4ce61780bd
Troy Moreland <49797484+34fame@users.noreply.github.com>
2020-08-23 02:25:14 -0500

    chore: changing config to use publicPath build option

--- quasar.conf.js
+++ quasar.conf.js
@@ -45,23 +45,8 @@ module.exports = function (/* ctx */) {
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
-      // transpile: false,
+      publicPath: 'q-reminder-dialog',
 
-      // Add dependencies for transpiling with Babel (Array of string/regex)
-      // (from node_modules, which are by default not transpiled).
-      // Applies only if "transpile" is set to true.
-      // transpileDependencies: [],
-
-      // rtl: false, // https://quasar.dev/options/rtl-support
-      // preloadChunks: true,
-      // showProgress: false,
-      // gzip: true,
-      // analyze: true,
-
-      // Options below are automatically set depending on the env, set them if you want to override
-      // extractCSS: false,
-
-      // https://quasar.dev/quasar-cli/handling-webpack
       extendWebpack (cfg) {
       },
     },
